b55fba2822a2110f314d806fa947b1d8322e60bd
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-25 19:51:40 -0600

    another push to Heroku

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
